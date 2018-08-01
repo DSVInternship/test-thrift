f39597b317e9999949154e3497e81e2133642d81
nhatvm <minhnhatse.uit@gmail.com>
2018-08-01 18:21:34 +0700

    first commit

---
+++ src/main/java/com/mycompany/thrift/test/MultiplicationHandler.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.thrift.test;
+
+import org.apache.thrift.TException;
+
+/**
+ *
+ * @author root
+ */
+public class MultiplicationHandler implements MultiplicationService.Iface{
+    @Override
+	 public int multiply(int n1, int n2) throws TException {
+	    System.out.println("Multiply(" + n1 + "," + n2 + ")");
+	    return n1 * n2;
+	 }
+}
