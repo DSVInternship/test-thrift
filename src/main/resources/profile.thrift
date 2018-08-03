c882306a5543f6e4a77c3e0c2d7f22427656ff0e
nhatvm <minhnhatse.uit@gmail.com>
2018-08-03 15:22:22 +0700

    add restfull, not complete

---
+++ src/main/resources/profile.thrift
@@ -0,0 +1,44 @@
+namespace java example.thrift
+
+typedef i32 int // We can use typedef to get pretty names for the types we are using
+typedef i64 long
+
+/*
+	User Profile struct
+*/
+struct Profile {
+	1:long id,
+	2:string name,
+	3:int age
+}
+struct stuent 
+
+struct TGetProfileRes 
+{
+	1: required i64 err,
+	2: optional Profile profile,	
+}
+
+service ProfileService {
+	list<Profile> getAll(),
+	Profile get(1:long id),
+	bool insert(1:Profile profile),
+	bool update(1:Profile profile),
+	bool remove(1:long id),
+}
+
+service ProfileExtService {
+	list<Profile> getAll(),
+	Profile get(1:long id),
+	bool insert(1:Profile profile),
+	bool update(1:Profile profile),
+	bool remove(1:long id),
+}
+
+/*
+service MultiplicationService
+{
+        int multiply(1:int n1, 2:int n2),
+        int add(1:int n1, 2:int n2),
+}
+*/
